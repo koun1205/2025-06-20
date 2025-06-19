9984c33861c6ed100b6d9a1736beea4cb9ecc5f6
koun1205 <z0905963606@gmail.com>
2025-06-19 21:17:13 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+﻿const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 300,
+        height: 300,
+        frame: false,
+        transparent: true,
+        alwaysOnTop: true,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+        },
+    });
+
+    win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
